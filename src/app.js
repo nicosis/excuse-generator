61db70c9f93bc010d2780d3a8151a8307b8d05f8
nicosis <nicosis@gmail.com>
2023-03-07 08:12:15 +0000

    correccion sandra

--- src/app.js
+++ src/app.js
@@ -8,9 +8,31 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-  function excuseGenerator() {
+  // function excuseGenerator() {
+  //   let who = ["the dog", "my granma", "his turtle", "my bird"];
+  //   let what = ["eat", "pissed", "crushed", "broked"];
+  //   let when = [
+  //     "before the class",
+  //     "right in time",
+  //     "when I finished",
+  //     "during my lunch",
+  //     "while I was praying"
+  //   ];
+
+  //   let excuseWho = who[Math.floor(Math.random() * who.length)];
+  //   let excuseWhat = what[Math.floor(Math.random() * what.length)];
+  //   let excuseWhen = when[Math.floor(Math.random() * when.length)];
+
+  //   let result = `${excuseWho} ${excuseWhat} ${excuseWhen}.`;
+
+  //   return result;
+  // };
+
+  //sandra
+
+  let generateExcuse = () => {
     let who = ["the dog", "my granma", "his turtle", "my bird"];
-    let what = ["eat", "pissed", "crushed", "broked"];
+    let what = ["ate", "pissed", "crushed", "broked"];
     let when = [
       "before the class",
       "right in time",
@@ -18,16 +40,16 @@ window.onload = function() {
       "during my lunch",
       "while I was praying"
     ];
-
+    //Hacer que esa excusa muestre de forma aleatoria cada elemento del who, what, when
     let excuseWho = who[Math.floor(Math.random() * who.length)];
     let excuseWhat = what[Math.floor(Math.random() * what.length)];
     let excuseWhen = when[Math.floor(Math.random() * when.length)];
 
-    let result = `${excuseWho} ${excuseWhat} ${excuseWhen}.`;
+    let resultado = excuseWho + " " + excuseWhat + " " + excuseWho + ".";
 
-    return result;
-  }
+    return resultado;
+  };
 
   console.log("Hello Rigo from the console!");
-  document.querySelector("#excuse").innerHTML = excuseGenerator();
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
