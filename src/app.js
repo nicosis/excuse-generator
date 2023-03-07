1967ccff0809d71faddc88464be9d4aa6ab63173
nicosis <nicosis@gmail.com>
2023-03-07 08:33:12 +0000

    Revert "correccion sandra"
    This reverts commit 61db70c9f93bc010d2780d3a8151a8307b8d05f8.

--- src/app.js
+++ src/app.js
@@ -8,31 +8,9 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-  // function excuseGenerator() {
-  //   let who = ["the dog", "my granma", "his turtle", "my bird"];
-  //   let what = ["eat", "pissed", "crushed", "broked"];
-  //   let when = [
-  //     "before the class",
-  //     "right in time",
-  //     "when I finished",
-  //     "during my lunch",
-  //     "while I was praying"
-  //   ];
-
-  //   let excuseWho = who[Math.floor(Math.random() * who.length)];
-  //   let excuseWhat = what[Math.floor(Math.random() * what.length)];
-  //   let excuseWhen = when[Math.floor(Math.random() * when.length)];
-
-  //   let result = `${excuseWho} ${excuseWhat} ${excuseWhen}.`;
-
-  //   return result;
-  // };
-
-  //sandra
-
-  let generateExcuse = () => {
+  function excuseGenerator() {
     let who = ["the dog", "my granma", "his turtle", "my bird"];
-    let what = ["ate", "pissed", "crushed", "broked"];
+    let what = ["eat", "pissed", "crushed", "broked"];
     let when = [
       "before the class",
       "right in time",
@@ -40,16 +18,16 @@ window.onload = function() {
       "during my lunch",
       "while I was praying"
     ];
-    //Hacer que esa excusa muestre de forma aleatoria cada elemento del who, what, when
+
     let excuseWho = who[Math.floor(Math.random() * who.length)];
     let excuseWhat = what[Math.floor(Math.random() * what.length)];
     let excuseWhen = when[Math.floor(Math.random() * when.length)];
 
-    let resultado = excuseWho + " " + excuseWhat + " " + excuseWho + ".";
+    let result = `${excuseWho} ${excuseWhat} ${excuseWhen}.`;
 
-    return resultado;
-  };
+    return result;
+  }
 
   console.log("Hello Rigo from the console!");
-  document.querySelector("#excuse").innerHTML = generateExcuse();
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
 };
